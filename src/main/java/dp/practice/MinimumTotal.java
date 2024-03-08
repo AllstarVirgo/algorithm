b0aae9b2f2ed5c1554efd2bd35b626af40b77324
wangyongxing <wangyongxing@idea.edu.cn>
2024-03-08 09:45:29 +0800

    opt

--- src/main/java/dp/practice/MinimumTotal.java
+++ src/main/java/dp/practice/MinimumTotal.java
@@ -1,4 +1,4 @@
-package graph.practice;
+package dp.practice;
 
 import java.util.List;
 
@@ -6,24 +6,29 @@ public class MinimumTotal {
     public int minimumTotal(List<List<Integer>> triangle) {
         int m = triangle.size();
         int n = triangle.get(triangle.size() - 1).size();
-        int[][] min = new int[m][n];
+        int[][] min = new int[2][n];
         min[0][0] = triangle.get(0).get(0);
         if (m == 1) {
             return triangle.get(0).get(0);
         }
+        int cur = 1, prev = 0;
         int res = Integer.MAX_VALUE;
         for (int i = 1; i < m; i++) {
             for (int j = 0; j <= i; j++) {
+                final int modCur = cur % 2;
+                final int modPrev = prev % 2;
                 if (j == 0) {
-                    min[i][j] = min[i - 1][j] + triangle.get(i).get(j);
+                    min[modCur][j] = min[modPrev][j] + triangle.get(i).get(j);
                 } else if (j == i) {
-                    min[i][j] = min[i - 1][j - 1] + triangle.get(i).get(j);
+                    min[modCur][j] = min[modPrev][j - 1] + triangle.get(i).get(j);
                 } else
-                    min[i][j] = Math.min(min[i - 1][j] + triangle.get(i).get(j), min[i - 1][j - 1] + triangle.get(i).get(j));
+                    min[modCur][j] = Math.min(min[modPrev][j], min[modPrev][j - 1]) + +triangle.get(i).get(j);
                 if (i == m - 1) {
-                    res = Math.min(res, min[i][j]);
+                    res = Math.min(res, min[modCur][j]);
                 }
             }
+            cur++;
+            prev++;
         }
         return res;
     }
